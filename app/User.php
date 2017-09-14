2b0be583e1af2ea912ddc4f59cbf4df0175f8797
kreativjamesz <kreativjames@gmail.com>
2017-09-15 07:07:53 +0800

    Second Commit

--- app/User.php
+++ app/User.php
@@ -4,11 +4,12 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laratrust\Traits\LaratrustTraits;
 
 class User extends Authenticatable
 {
     use Notifiable;
-
+    use LaratrustUserTraits;
     /**
      * The attributes that are mass assignable.
      *
